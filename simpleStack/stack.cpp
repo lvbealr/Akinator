b3a2fcdb601022e367d9ba40ccbbbfdb165669e4
lvbealr <lvbealr@mail.ru>
2024-11-18 20:02:36 +0300

    simple stack on char**

---
+++ simpleStack/stack.cpp
@@ -0,0 +1,82 @@
+#include <cstdlib>
+
+#include "stack.h"
+
+stackError stackInitialize (Stack *stack, int capacity) {
+  customWarning(stack    != NULL, STACK_NULL_POINTER);
+  customWarning(capacity >= 0,    BAD_CAPACITY      );
+
+  stack->size     = 0;
+  stack->capacity = capacity;
+  stack->data     = (char **)calloc(capacity, sizeof(char *));
+
+  customWarning(stack->data != NULL, BAD_DATA_POINTER);
+
+  return STACK_NO_ERROR;
+}
+
+stackError stackDestruct   (Stack *stack) {
+  customWarning(stack != NULL, STACK_NULL_POINTER);
+
+  stack->size     = -1;
+  stack->capacity = -1;
+
+  FREE_(stack->data);
+
+  return STACK_NO_ERROR;
+}
+
+stackError stackPush       (Stack *stack, char *data) {
+  customWarning(stack != NULL, STACK_NULL_POINTER);
+  customWarning(data  != NULL, BAD_DATA_POINTER  );
+
+  if (stack->size >= stack->capacity) {
+    stackResize(stack, ADD_MEMORY);
+  }
+
+  stack->data[stack->size++] = data;
+  return STACK_NO_ERROR;
+}
+
+stackError stackPop        (Stack *stack, char **dataPointer) {
+  customWarning(stack          != NULL, STACK_NULL_POINTER);
+  customWarning(dataPointer    != NULL, BAD_DATA_POINTER  );
+  customWarning(stack->size    >     0, BAD_SIZE          );
+
+  if (stack->size <= stack->capacity / 4) {
+    stackResize(stack, DUMP_MEMORY);
+  }
+
+  (*dataPointer)           = stack->data[--stack->size];
+  stack->data[stack->size] = NULL;
+
+  return STACK_NO_ERROR;
+}
+
+stackError stackResize(Stack *stack, changeMemory mode) {
+  customWarning(stack != NULL, STACK_NULL_POINTER);
+
+  switch (mode) {
+    case ADD_MEMORY:
+      {
+        int newCapacity = stack->capacity * 2;
+
+        stack->data = (char **)realloc(stack->data, sizeof(char *) * (size_t)newCapacity);
+        customWarning(stack->data != NULL, BAD_DATA_POINTER);
+
+        stack->capacity *= 2;
+      }
+
+    case DUMP_MEMORY:
+      {
+        int newCapacity = stack->capacity / 2;
+
+        stack->data = (char **)realloc(stack->data, sizeof(char *) * (size_t)newCapacity);
+        customWarning(stack->data != NULL, BAD_DATA_POINTER);
+
+        stack->capacity /= 2;
+      }
+  }
+
+  return STACK_NO_ERROR;
+}
---
+++ simpleStack/stack.h
@@ -0,0 +1,32 @@
+#include "customWarning.h"
+
+#define FREE_(field) { \
+  free(field);         \
+  field = NULL;        \
+}
+
+struct Stack {
+  int    capacity = {};
+  int    size     = {};
+  char **data     = {};
+};
+
+enum stackError {
+  STACK_NO_ERROR     = 0,
+  BAD_CAPACITY       = 1,
+  BAD_SIZE           = 2,
+  BAD_DATA_POINTER   = 3,
+  BAD_POP_POINTER    = 4,
+  STACK_NULL_POINTER = 5
+};
+
+enum changeMemory {
+  DUMP_MEMORY = -1,
+  ADD_MEMORY  =  1
+};
+
+stackError stackInitialize (Stack *stack, int    capacity   );
+stackError stackDestruct   (Stack *stack                    );
+stackError stackPush       (Stack *stack, char  *data       );
+stackError stackPop        (Stack *stack, char **dataPointer);
+stackError stackResize     (Stack *stack, changeMemory mode );
